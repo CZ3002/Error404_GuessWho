c04c8e150445299e5e195734f5db2515a8824d15
ShreyasMundhra <shreyass001@e.ntu.edu.sg>
2016-10-02 23:49:17 +0800

    Logout button

--- app/src/main/java/com/example/shrey_000/guesswho/HomeActivity.java
+++ app/src/main/java/com/example/shrey_000/guesswho/HomeActivity.java
@@ -1,13 +1,15 @@
 package com.example.shrey_000.guesswho;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.shrey_000.guesswho.FaceGame.FaceGameActivity;
 import com.example.shrey_000.guesswho.PersonalCollection.PersonalCollectionActivity;
-import com.example.shrey_000.guesswho.PersonalCollection.ViewPersonalCollectionActivity;
 import com.example.shrey_000.guesswho.VoiceGame.VoiceGameActivity;
 
 public class HomeActivity extends AppCompatActivity {
@@ -31,5 +33,30 @@ public class HomeActivity extends AppCompatActivity {
     public void onViewPersonalCollection(View view) {
         Intent PCollection = new Intent(this,PersonalCollectionActivity.class);
         startActivity(PCollection);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_logoutHome) {
+            SharedPreferences prefs = getSharedPreferences("CZ3002Prefs", MODE_MULTI_PROCESS);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.remove("loginId");
+            editor.commit();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
